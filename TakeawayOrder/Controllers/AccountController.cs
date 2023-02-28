6c303dc2bd657c489cd647c76940a3f1f59c05f8
jamorajj <jamorajj@gmail.com>
2023-02-28 21:05:24 +1300

    add initial login controller and view

---
+++ TakeawayOrder/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TakeawayOrder.Models;
+
+namespace TakeawayOrder.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<IdentityUser> _signInManager;
+        private UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(loginVM.UserName, loginVM.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return Redirect("/");
+                }
+
+                ModelState.AddModelError("", "Invalid username or password");
+            }
+
+            return View(loginVM);
+        }
+    }
+}
---
+++ TakeawayOrder/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TakeawayOrder.Models
+{
+    public class LoginViewModel
+    {
+        [Required, MinLength(2, ErrorMessage = "Min length is 2")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        [DataType(DataType.Password), Required, MinLength(4, ErrorMessage = "Min length is 4")]
+        public string Password { get; set; }
+    }
+}
