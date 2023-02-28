5b1b680f161ac3ff784f251fd302769e96c5eb9e
jamorajj <jamorajj@gmail.com>
2023-02-28 16:20:08 +1300

    initialize identity

---
+++ TakeawayOrder/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TakeawayOrder.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        [Required, MinLength(2, ErrorMessage = "Min length is 2")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password), Required, MinLength(4, ErrorMessage = "Min length is 4")]
+        public string Password { get; set; }
+    }
+}
--- TakeawayOrder/Program.cs
+++ TakeawayOrder/Program.cs
@@ -1,7 +1,40 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TakeawayOrder.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add configuration for weak password to make testing easier
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 4;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    options.User.RequireUniqueEmail = true;
+});
+
+// Add configuration for login and access denied path
+builder.Services.Configure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/AccessDenied";
+});
+
+// Add Razor Pages
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
