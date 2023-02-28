4c5895aa0039747cad962beda94016b5f8f56a8d
jamorajj <jamorajj@gmail.com>
2023-02-28 15:07:21 +1300

    Add project files.

---
+++ TakeawayOrder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TakeawayOrder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
