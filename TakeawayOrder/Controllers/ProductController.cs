633c334d267e38ffa32e8e6d6259d7c617b9350f
jamorajj <jamorajj@gmail.com>
2023-03-01 20:35:40 +1300

    product crud

--- TakeawayOrder/Controllers/ProductController.cs
+++ TakeawayOrder/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 using TakeawayOrder.Data;
+using TakeawayOrder.Models;
 
 namespace TakeawayOrder.Controllers
 {
@@ -12,9 +16,57 @@ namespace TakeawayOrder.Controllers
         {
             _context = context;
         }
+        [Authorize(Roles = "Kitchen,Admin")]
         public IActionResult Index()
         {
             return View(_context.Products.ToList());
+        }
+        [Authorize(Roles = "Kitchen,Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        [Authorize(Roles = "Kitchen,Admin")]
+        public async Task<IActionResult> Edit(long id)
+        {
+            Product product = await _context.Products.FindAsync(id);
+
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        public async Task<IActionResult> Delete(long id)
+        {
+            Product product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+
         }
     }
 }
